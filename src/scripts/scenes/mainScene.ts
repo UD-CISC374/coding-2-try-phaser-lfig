484959b620a6fd10ff6052c2a7b77c72d864d195
Luis Figueroa <lfig@udel.edu>
2020-02-27 10:19:28 -0500

    commit

--- src/scripts/scenes/mainScene.ts
+++ src/scripts/scenes/mainScene.ts
@@ -8,6 +8,12 @@ export default class MainScene extends Phaser.Scene {
   }
 
   create() {
+    let background = this.add.image(0,0,"background");
+    background.setOrigin(0,0);
+    let ship1 = this.add.image(400/2-50, 400/2, "ship");
+    let ship2 = this.add.image(400/2, 400/2, "ship2");
+    let ship3 = this.add.image(400/2+50, 400/2, "ship3");
+    this.add.text(20,20,"Playing game", {font: "25px Arial", fill: "yellow"});
     this.exampleObject = new ExampleObject(this, 0, 0);
   }
 
--- src/scripts/scenes/preloadScene.ts
+++ src/scripts/scenes/preloadScene.ts
@@ -4,9 +4,14 @@ export default class PreloadScene extends Phaser.Scene {
   }
 
   preload() {
+    this.load.image("background", "C:\CISC 374\getting-started-with-phaser-master\getting-started-with-phaser-master\part 3 Game Objects - Images\assets\images\background.png");
+    this.load.image("ship","C:\CISC 374\getting-started-with-phaser-master\getting-started-with-phaser-master\part 3 Game Objects - Images\assets\images\ship.png");
+    this.load.image("ship2","C:\CISC 374\getting-started-with-phaser-master\getting-started-with-phaser-master\part 3 Game Objects - Images\assets\images\ship2.png");
+    this.load.image("ship3","C:\CISC 374\getting-started-with-phaser-master\getting-started-with-phaser-master\part 3 Game Objects - Images\assets\images\ship3.png");
   }
 
   create() {
+    this.add.text(20,20,"Loading game...");
     this.scene.start('MainScene');
   }
 }
